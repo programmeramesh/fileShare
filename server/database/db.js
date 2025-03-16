7373a7d7f0f3e87e92100fdc27ca942d51093e5b
programmeramesh <O24MCA160445@cuchd.in>
2025-03-16 20:02:14 +0530

    Initial commit

---
+++ server/database/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DBConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+
+export default DBConnection;
---
+++ server/routes/routes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import upload from '../utils/upload.js';
+import { uploadImage, getImage } from '../controller/image-controller.js';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the File Sharing API' });
+});
+
+router.post('/upload', upload.single('file'), uploadImage);
+router.get('/file/:fileId', getImage);
+
+export default router;
